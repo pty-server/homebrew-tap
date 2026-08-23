9ca5d50ce30972de1a052c5be3890513aa391494
choux-release <noreply@pty-server.dev>
2026-08-23 20:49:32 +0000

    chore(choux): 0.1.0

--- Casks/choux.rb
+++ Casks/choux.rb
@@ -1,6 +1,6 @@
 cask "choux" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "0.1.0"
+  sha256 "1149f5a6a32840e60d53e25fa02d1655402eee846cb81e41c49bcaf59b65f168"
 
   url "https://github.com/pty-server/choux/releases/download/v#{version}/choux_#{version}_aarch64.dmg"
   name "choux"
